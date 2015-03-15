feeca791ee8b4111d4cf5be1ae6b3f56ec5c0818
Laniax <bnoorlander@hotmail.com>
2015-03-15 23:05:09 +0100

    Don't loot items we cannot reach.

--- Looting.java
+++ Looting.java
@@ -56,6 +56,9 @@ public class Looting {
 				
 				if (Inventory.isFull())
 					return false;
+				
+				if (!Movement.canReach(item))
+					continue;
 
 				Camera.turnToTile(item);
 				item.setClickHeight(addHeight);
