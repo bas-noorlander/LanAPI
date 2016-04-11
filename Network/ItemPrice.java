40256809903f1711ed02525bf55903017d595a90
Laniax <bnoorlander@hotmail.com>
2016-04-11 21:59:22 +0200

    Correctlt get the value for coins ;)

--- Network/ItemPrice.java
+++ Network/ItemPrice.java
@@ -2,7 +2,6 @@ package scripts.LanAPI.Network;
 
 import scripts.LanAPI.Core.Dynamic.Bag;
 import scripts.LanAPI.Core.IO.JSON.Json;
-import scripts.LanAPI.Core.IO.JSON.JsonObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,11 +15,17 @@ public class ItemPrice {
 
     private static Bag bag = new Bag();
 
+    private final static int ITEMID_COINS = 995;
+
     public static int get(final int itemId) {
 
         Integer cachedPrice = bag.get(String.valueOf(itemId));
         if (cachedPrice != null)
             return cachedPrice;
+
+        // Coins don't have a sell price ;) but lets count them anyway
+        if (itemId == ITEMID_COINS)
+            return 1;
 
         try {
             URL url = new URL("https://api.rsbuddy.com/grandExchange?a=guidePrice&i=" + itemId);
